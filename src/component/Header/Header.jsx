1bec66aa5667fe5161c82fdd7e1a0b886bbb1cf3
dat.truong <truongquocdat196nt@gmail.com>
2022-01-02 07:50:31 +0700

    chore: retype-header-css

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,12 +1,14 @@
 const Header = ({counter = 0}) => {
 	return (
+		<>
 		<div className='header'>
-			<div className='container'>
+			<div className='nav'>
 				<div className='btn btn--primary'>Home</div>
 				<div className='btn btn--primary'>Count</div>
 			</div>
-			<p className='counter'>Count: {counter}</p>
-	</div>
+			<p className='header__right counter'>Count: {counter}</p>
+		</div>
+		</>
 	)
 }
 
