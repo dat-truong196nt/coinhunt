9cdcfb2b896cba9133ac69e8e30db3aa8a2c7bdc
dat.truong <truongquocdat196nt@gmail.com>
2022-01-02 10:59:58 +0700

    feat: add-coin-pattern

---
+++ src/component/Coin/Coin.jsx
@@ -0,0 +1,13 @@
+const Coin = ({imgUrl='https://s2.coinmarketcap.com/static/img/coins/200x200/1839.png', price='$0'}) => (
+	<div className='coin'>
+		<div className='coin-info'>
+			<img className='coin-info__image' src={imgUrl} alt='coin'></img>
+			<div className='coin-info__price'>{price}</div>
+		</div>
+		<div className='coin-action'>
+			<button className='btn btn--primary coin-update__btn'>Force Update</button>
+		</div>
+	</div>
+)
+
+export default Coin;
