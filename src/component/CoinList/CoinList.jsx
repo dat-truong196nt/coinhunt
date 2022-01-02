95e5d7567b97b059d22e17af4c9a4a9f81c50b81
dat.truong <truongquocdat196nt@gmail.com>
2022-01-03 06:07:43 +0700

    chore: update-loading-param

--- src/component/CoinList/CoinList.jsx
+++ src/component/CoinList/CoinList.jsx
@@ -1,9 +1,9 @@
 import Coin from "../Coin/Coin"
 
-const CoinList = ({coins}) => {
+const CoinList = ({coins, isLoading}) => {
 	return (
 		<div className='coin-list'>
-			{coins.map((coin) => <Coin key={coin.id} imgUrl={coin.image} price={coin.current_price}/>)}
+			{coins.map((coin) => <Coin key={coin.id} isLoading={isLoading} imgUrl={coin.image} price={coin.current_price}/>)}
 		</div>
 	)
 }
