93a4de3ec968dcc5dd00f6caeaa7a1c5398b9875
dat.truong <truongquocdat196nt@gmail.com>
2022-01-02 20:33:45 +0700

    chore: add-price-unit

--- src/component/Coin/Coin.jsx
+++ src/component/Coin/Coin.jsx
@@ -2,7 +2,7 @@ const Coin = ({imgUrl='https://s2.coinmarketcap.com/static/img/coins/200x200/183
 	<div className='coin'>
 		<div className='coin-info'>
 			<img className='coin-info__image' src={imgUrl} alt='coin'></img>
-			<div className='coin-info__price'>{price}</div>
+			<div className='coin-info__price'>${price}</div>
 		</div>
 		<div className='coin-action'>
 			<button className='btn btn--primary coin-action__btn'>Force Update</button>
